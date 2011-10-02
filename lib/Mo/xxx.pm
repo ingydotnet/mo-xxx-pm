c358578b8fa8b911f22e611b9d49815e290ca0e0
Ingy dot Net <ingy@ingy.net>
2011-10-02 02:01:00 +0100

    code

--- lib/Mo/xxx.pm
+++ lib/Mo/xxx.pm
@@ -1,22 +1,3 @@
-##
-# name:      Mo::xxx
-# abstract:  
-# author:    Ingy döt Net <ingy@ingy.net>
-# license:   perl
-# copyright: 2011
-
-use 5.010;
-use strict;
-use warnings;
-
-package Mo::xxx;
-
-1;
-
-=head1 SYNOPSIS
-
-...
-
-=head1 DESCRIPTION
-
-...
+package Mo::xxx;my$M="Mo::";
+$VERSION=0.25;
+use constant XXX_skip=>1;${$M.'::DumpModule'}='YAML::XS';*{$M.'xxx::e'}=sub{my($P,$e)=@_;$e->{WWW}=sub{require XXX;local$XXX::DumpModule=${$M.'::DumpModule'};XXX::WWW(@_)};$e->{XXX}=sub{require XXX;local$XXX::DumpModule=${$M.'::DumpModule'};XXX::XXX(@_)};$e->{YYY}=sub{require XXX;local$XXX::DumpModule=${$M.'::DumpModule'};XXX::YYY(@_)};$e->{ZZZ}=sub{require XXX;local$XXX::DumpModule=${$M.'::DumpModule'};XXX::ZZZ(@_)}};
---
+++ src/Mo/xxx.pm
@@ -0,0 +1,17 @@
+package Mo::xxx;
+my $MoPKG = "Mo::";
+$VERSION = 0.10;
+
+use constant XXX_skip => 1;
+${$MoPKG.'::DumpModule'} = 'YAML::XS';
+*{$MoPKG.'xxx::e'} = sub {
+    my ($caller_pkg, $exports) = @_;
+    $exports->{WWW} = sub { require XXX; local $XXX::DumpModule =
+        ${$MoPKG.'::DumpModule'}; XXX::WWW(@_) };
+    $exports->{XXX} = sub { require XXX; local $XXX::DumpModule =
+        ${$MoPKG.'::DumpModule'}; XXX::XXX(@_) };
+    $exports->{YYY} = sub { require XXX; local $XXX::DumpModule =
+        ${$MoPKG.'::DumpModule'}; XXX::YYY(@_) };
+    $exports->{ZZZ} = sub { require XXX; local $XXX::DumpModule =
+        ${$MoPKG.'::DumpModule'}; XXX::ZZZ(@_) };
+};
