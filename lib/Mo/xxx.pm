395f9c633ea522b8918c8b96dcbbb92c7db7cd14
Ingy dot Net <ingy@ingy.net>
2011-10-02 01:46:35 +0100

    First commit

---
+++ lib/Mo/xxx.pm
@@ -0,0 +1,22 @@
+##
+# name:      Mo::xxx
+# abstract:  
+# author:    Ingy döt Net <ingy@ingy.net>
+# license:   perl
+# copyright: 2011
+
+use 5.010;
+use strict;
+use warnings;
+
+package Mo::xxx;
+
+1;
+
+=head1 SYNOPSIS
+
+...
+
+=head1 DESCRIPTION
+
+...
